0421ed37799e8927c9227176c68dc5aab908afd8
janenyasoro <janenyasoro@gmail.com>
2016-10-31 09:19:45 +0300

    finish log in/log out

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -9,7 +9,7 @@ assert_equal @user, current_user
 assert is_logged_in?
 end
 test "current_user returns nil when remember digest is wrong" do
-@user.update_attribute(:remember_digest, User.digest(User.new_token))
-assert_nil current_user
-end
+  @user.update_attribute(:remember_digest, User.digest(User.new_token))
+  assert_nil current_user
+ end
 end
